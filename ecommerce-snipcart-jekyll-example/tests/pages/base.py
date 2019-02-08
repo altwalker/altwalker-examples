b68bc7ffc3be07aeb99c4b56930d197690b84294
Dorin Oltean <dorinoltean@gmail.com>
2019-02-08 12:07:41 +0200

    wait for snipcart initialization

--- ecommerce-snipcart-jekyll-example/tests/pages/base.py
+++ ecommerce-snipcart-jekyll-example/tests/pages/base.py
@@ -1,6 +1,16 @@
 from pypom import Page
 
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class snipcart_initialized():
+    """An expectation for checking if snipcart is initialized"""
+
+    def __call__(self, driver):
+        return driver.execute_script("return Snipcart._initialized && Snipcart.ready;")
 
 
 class BasePage(Page):
@@ -19,7 +29,12 @@ class BasePage(Page):
     def is_cart_open(self):
         return self.is_element_present(*self.cart_locator)
 
+    def items_in_cart(self):
+        return int(
+            self.find_element(*self.cart_button_locator).find_element(By.CSS_SELECTOR, ".snipcart-total-items").text)
+
     def click_cart_button(self):
+        WebDriverWait(self.driver, 10).until(snipcart_initialized())
         self.find_element(*self.cart_button_locator).click()
 
     def click_close_cart_button(self):
@@ -27,3 +42,8 @@ class BasePage(Page):
 
     def click_home_button(self):
         self.find_element(*self.home_button_locator).click()
+
+    def wait_for_cart_reload(self):
+        element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "#snipcart-sub-content"))
+        )
--- ecommerce-snipcart-jekyll-example/tests/test.py
+++ ecommerce-snipcart-jekyll-example/tests/test.py
@@ -18,7 +18,7 @@ def setUpRun():
     driver = webdriver.Firefox(options=options)
 
     print("Implicity wait and maximize the window")
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(15)
     driver.maximize_window()
 
 
@@ -36,48 +36,88 @@ class NavigationModel(unittest.TestCase):
         print("Set up for Navigation model")
         self.driver = driver
 
-    def edge_loadHomePage(self):
+    def load_home_page(self):
         print("Load the Ecommerce home page")
-        home_page = HomePage(self.driver, "https://dorinoltean.github.io/snipcart-jekyll-integration/")
+        home_page = HomePage(self.driver, "https://dorinaltom.gitlab.io/snipcart-jekyll-ecommerce-demo/")
         home_page.open()
 
-    def edge_addToCartFromHomePage(self):
-        home_page = HomePage(self.driver)
-        home_page.add_to_cart_random_product()
+    def add_to_cart_from_homepage(self):
+        page = HomePage(self.driver)
+        page.add_to_cart_random_product()
+        page.wait_for_cart_reload()
 
-    def edge_navigateToProductPage(self):
+    def go_to_product_page(self):
         home_page = HomePage(self.driver)
         home_page.click_random_product()
 
-    def edge_closeCart(self):
+    def close_cart(self):
         page = BasePage(self.driver)
         page.click_close_cart_button()
 
-    def edge_goToHomePage(self):
+    def go_to_homepage(self):
         page = BasePage(self.driver)
         page.click_home_button()
 
-    def edge_addToCartFromProductPage(self):
+    def add_to_cart_from_product_page(self):
         page = ProductPage(self.driver)
         page.add_to_cart_click()
+        page.wait_for_cart_reload()
+
+    def open_cart(self):
+        page = BasePage(self.driver)
+        page.click_cart_button()
+
+    def do_nothing(self):
+        pass
 
     # vertices
 
-    def vertex_homepage(self):
+    def homepage(self):
         home_page = HomePage(self.driver)
         self.assertTrue(home_page.is_products_list_present())
 
-    def vertex_productPage(self):
+    def product_page(self):
         page = ProductPage(self.driver)
         self.assertTrue(page.is_product_page())
 
-    def vertex_homepageCartOpen(self):
+    def homepage_cart_open(self):
         page = BasePage(self.driver)
         self.assertTrue(page.is_cart_open())
 
-    def vertex_pageCartOpen(self):
+    def product_page_cart_open(self):
         page = BasePage(self.driver)
         self.assertTrue(page.is_cart_open())
+
+    def cart_open_and_not_empty(self):
+        page = BasePage(self.driver)
+        self.assertTrue(page.is_cart_open())
+        self.assertTrue(page.items_in_cart() > 0)
+
+
+class CheckoutModel(unittest.TestCase):
+    def setUpModel(self):
+        global driver
+
+        print("Set up for Navigation model")
+        self.driver = driver
+
+    # edges - actions
+
+    def finish_checkout(self):
+        page = BasePage(self.driver)
+        page.click_close_cart_button()
+        page.click_home_button()
+
+    # vertices - states
+
+    def cart_open_and_not_empty(self):
+        page = BasePage(self.driver)
+        self.assertTrue(page.is_cart_open())
+        self.assertTrue(page.items_in_cart() > 0)
+
+    def homepage(self):
+        home_page = HomePage(self.driver)
+        self.assertTrue(home_page.is_products_list_present())
 
 
 # if __name__ == "__main__":
