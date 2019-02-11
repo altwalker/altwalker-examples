80acd6a57cd2b51aa97f52ad42ff11f6164468ae
Dorin Oltean <dorinoltean@gmail.com>
2019-02-11 17:37:58 +0200

    added checkout steps

--- ecommerce-snipcart-jekyll-example/tests/pages/base.py
+++ ecommerce-snipcart-jekyll-example/tests/pages/base.py
@@ -1,6 +1,4 @@
 from pypom import Page
-
-from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,9 +14,14 @@ class snipcart_initialized():
 class BasePage(Page):
     """ Interact with elements commnon on every page """
     cart_button_locator = (By.CSS_SELECTOR, "header .snipcart-checkout")
-    cart_locator = (By.ID, "snip-layout-cart-content")
+    cart_locator = (By.ID, "snipcart-main-content")
     cart_close_button_locator = (By.ID, "snipcart-close")
     home_button_locator = (By.CSS_SELECTOR, "header .site-title")
+    total_items_in_cart_locator = (By.CSS_SELECTOR, ".snipcart-total-items")
+    cart_items_list_holder_locator = (By.CSS_SELECTOR, "#snipcart-sub-content")
+
+    def wait_for_snipcart_initialization(self):
+        WebDriverWait(self.driver, 10).until(snipcart_initialized())
 
     def __init__(self, selenium, base_url=None, timeout=10, **url_kwargs):
         return super().__init__(selenium, base_url=base_url, timeout=timeout, **url_kwargs)
@@ -30,11 +33,10 @@ class BasePage(Page):
         return self.is_element_present(*self.cart_locator)
 
     def items_in_cart(self):
-        return int(
-            self.find_element(*self.cart_button_locator).find_element(By.CSS_SELECTOR, ".snipcart-total-items").text)
+        return int(self.find_element(*self.cart_button_locator).find_element(*self.total_items_in_cart_locator).text)
 
     def click_cart_button(self):
-        WebDriverWait(self.driver, 10).until(snipcart_initialized())
+        self.wait_for_snipcart_initialization()
         self.find_element(*self.cart_button_locator).click()
 
     def click_close_cart_button(self):
@@ -45,5 +47,69 @@ class BasePage(Page):
 
     def wait_for_cart_reload(self):
         element = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, "#snipcart-sub-content"))
+            EC.visibility_of_element_located(self.cart_items_list_holder_locator)
         )
+
+
+class CartPage(BasePage):
+    """interact with element on the cart page"""
+    content_cart_next_step_button_locator = (By.CSS_SELECTOR, "#snipcart-main-content #snipcart-actions a")
+    billing_addres_next_step_button_locator = (By.ID, "snipcart-next")
+    payment_next_step_button_locator = (By.ID, "snipcart-paymentmethod-pay")
+    order_confirmation_place_order_button_locator = (By.CSS_SELECTOR, "a.js-submit")
+    billing_address_form_locator = (By.ID, "snipcart-billingaddress-form")
+
+    billing_name_locator = (By.ID, "snip-name")
+    billing_city_locator = (By.ID, "snip-city")
+    billing_email_locator = (By.ID, "snip-email")
+    billing_street_address1_locator = (By.ID, "snip-address1")
+    billing_postal_code_locator = (By.ID, "snip-postalCode")
+
+    def __init__(self, selenium, base_url=None, timeout=10, **url_kwargs):
+        return super().__init__(selenium, base_url=base_url, timeout=timeout, **url_kwargs)
+
+    def is_content_cart_view(self):
+        return self.is_element_present(*self.content_cart_next_step_button_locator)
+
+    def is_billing_cart_view(self):
+        return self.is_element_present(*self.billing_addres_next_step_button_locator)
+
+    def is_payment_cart_view(self):
+        return self.is_element_present(*self.payment_next_step_button_locator)
+
+    def is_order_confirmation_cart_view(self):
+        return self.is_element_present(*self.order_confirmation_place_order_button_locator)
+
+    def click_content_cart_next_step_button(self):
+        self.wait_for_snipcart_initialization()
+        self.find_element(*self.content_cart_next_step_button_locator).click()
+
+    def click_billing_addres_next_step_button(self):
+        self.wait_for_snipcart_initialization()
+        self.find_element(*self.billing_addres_next_step_button_locator).click()
+
+    def click_payment_next_step_button(self):
+        self.wait_for_snipcart_initialization()
+        self.find_element(*self.payment_next_step_button_locator).click()
+
+    def click_order_confirmation_place_order_button(self):
+        self.wait_for_snipcart_initialization()
+        self.find_element(*self.order_confirmation_place_order_button_locator).click()
+
+    def fill_in_billing_adress_form(self, name="", city="", email="", street_address1="", postal_code=""):
+        print("Fill Billing Form")
+        if (name != ""):
+            self.find_element(*self.billing_name_locator).clear()
+            self.find_element(*self.billing_name_locator).send_keys(name)
+        if (city != ""):
+            self.find_element(*self.billing_city_locator).clear()
+            self.find_element(*self.billing_city_locator).send_keys(city)
+        if email != "":
+            self.find_element(*self.billing_email_locator).clear()
+            self.find_element(*self.billing_email_locator).send_keys(email)
+        if street_address1 != "":
+            self.find_element(*self.billing_street_address1_locator).clear()
+            self.find_element(*self.billing_street_address1_locator).send_keys(street_address1)
+        if postal_code != "":
+            self.find_element(*self.billing_postal_code_locator).clear()
+            self.find_element(*self.billing_postal_code_locator).send_keys(postal_code)
--- ecommerce-snipcart-jekyll-example/tests/test.py
+++ ecommerce-snipcart-jekyll-example/tests/test.py
@@ -2,7 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-from tests.pages.base import BasePage
+from tests.pages.base import BasePage, CartPage
 from tests.pages.home import HomePage
 from tests.pages.product import ProductPage
 
@@ -29,7 +29,17 @@ def tearDownRun():
     driver.quit()
 
 
-class NavigationModel(unittest.TestCase):
+class BaseModel(unittest.TestCase):
+    def cart_open_and_not_empty(self):
+        page = CartPage(self.driver)
+        page.wait_for_snipcart_initialization()
+
+        self.assertTrue(page.is_cart_open())
+        self.assertTrue(page.items_in_cart() > 0)
+        self.assertTrue(page.is_content_cart_view())
+
+
+class NavigationModel(BaseModel):
     def setUpModel(self):
         global driver
 
@@ -38,8 +48,8 @@ class NavigationModel(unittest.TestCase):
 
     def load_home_page(self):
         print("Load the Ecommerce home page")
-        home_page = HomePage(self.driver, "https://dorinaltom.gitlab.io/snipcart-jekyll-ecommerce-demo/")
-        home_page.open()
+        page = HomePage(self.driver, "https://dorinaltom.gitlab.io/snipcart-jekyll-ecommerce-demo/")
+        page.open()
 
     def add_to_cart_from_homepage(self):
         page = HomePage(self.driver)
@@ -47,8 +57,8 @@ class NavigationModel(unittest.TestCase):
         page.wait_for_cart_reload()
 
     def go_to_product_page(self):
-        home_page = HomePage(self.driver)
-        home_page.click_random_product()
+        page = HomePage(self.driver)
+        page.click_random_product()
 
     def close_cart(self):
         page = BasePage(self.driver)
@@ -73,8 +83,8 @@ class NavigationModel(unittest.TestCase):
     # vertices
 
     def homepage(self):
-        home_page = HomePage(self.driver)
-        self.assertTrue(home_page.is_products_list_present())
+        page = HomePage(self.driver)
+        self.assertTrue(page.is_products_list_present())
 
     def product_page(self):
         page = ProductPage(self.driver)
@@ -88,13 +98,8 @@ class NavigationModel(unittest.TestCase):
         page = BasePage(self.driver)
         self.assertTrue(page.is_cart_open())
 
-    def cart_open_and_not_empty(self):
-        page = BasePage(self.driver)
-        self.assertTrue(page.is_cart_open())
-        self.assertTrue(page.items_in_cart() > 0)
 
-
-class CheckoutModel(unittest.TestCase):
+class CheckoutModel(BaseModel):
     def setUpModel(self):
         global driver
 
@@ -103,21 +108,49 @@ class CheckoutModel(unittest.TestCase):
 
     # edges - actions
 
-    def finish_checkout(self):
+    def go_to_billing_address(self):
+        page = CartPage(self.driver)
+        page.click_content_cart_next_step_button()
+
+    def fill_billing_and_go_to_payment(self):
+        page = CartPage(self.driver)
+        page.fill_in_billing_adress_form(name="Altwalker", city="Cluj-Napoca", email="hello@test.test",
+                                         postal_code=42012, street_address1="42 Cloud Street")
+        page.click_billing_addres_next_step_button()
+
+    def fill_payment_and_go_to_confirmation(self):
+        page = CartPage(self.driver)
+        page.click_payment_next_step_button()
+
+    def place_order(self):
+        page = CartPage(self.driver)
+        page.click_order_confirmation_place_order_button()
+
+    def go_to_homepage(self):
         page = BasePage(self.driver)
         page.click_close_cart_button()
         page.click_home_button()
 
     # vertices - states
 
-    def cart_open_and_not_empty(self):
-        page = BasePage(self.driver)
-        self.assertTrue(page.is_cart_open())
-        self.assertTrue(page.items_in_cart() > 0)
+    def billing_address(self):
+        page = CartPage(self.driver)
+        self.assertTrue(page.is_billing_cart_view())
+
+    def payment_method(self):
+        page = CartPage(self.driver)
+        self.assertTrue(page.is_payment_cart_view())
+
+    def order_confirmation(self):
+        page = CartPage(self.driver)
+        self.assertTrue(page.is_order_confirmation_cart_view())
+
+    def order_confirmed(self):
+        pass
 
     def homepage(self):
-        home_page = HomePage(self.driver)
-        self.assertTrue(home_page.is_products_list_present())
+        page = HomePage(self.driver)
+        self.assertTrue(page.is_products_list_present())
 
 
 # if __name__ == "__main__":
